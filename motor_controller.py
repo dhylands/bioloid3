21386d5628bac7961da8410dc8ef111ae1e8d668
Dave Hylands <dhylands@gmail.com>
2024-01-29 06:50:48 -0800

    Added motor_controller.py (WIP)

---
+++ motor_controller.py
@@ -0,0 +1,3 @@
+"""This module implements a motor controller as a Bioloid device.
+"""
+
---
+++ motor_controller_cfg.py
@@ -0,0 +1,5 @@
+# Register layout for motor_controller.
+
+ENCODER_L = 0x06    # 32-bits
+ENCODER_R = 0x0a    # 32-bits
+
